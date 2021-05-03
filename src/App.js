1dfc1f6c3174c62bac89e2cbcb38cda5f73c637f
Boris Descubes <38215510+EthicalChaos@users.noreply.github.com>
2021-05-03 21:20:36 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
-function App() {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+
+
+    console.log(id)
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? {
+          ...task, reminder:
+            !task.reminder
+        } : task
+      )
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Currently building this, give me a moment.
-        </p>
-        <a
-          className="Github"
-          href="https://github.com/EthicalChaos"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View my code projects
-        </a>
-      </header>
+    <div className="container">
+      <p>React.js UI Test number 1.</p>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={!showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ?
+        (<Tasks
+          tasks={tasks}
+          onDelete={deleteTask}
+          onToggle={toggleReminder} />)
+        : ('No tasks to show')
+      }
     </div>
   );
 }
+
 
 export default App;
